f5d128424e5ba5aba818f0614ec36526976e4416
edwincai <edwincai1995@126.com>
2017-07-18 16:47:42 +0800

    Add files via upload

---
+++ HW2_code/newPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace TODO
+{
+    public sealed partial class newPage : Page
+    {
+
+        public newPage()
+        {
+            this.InitializeComponent();
+            var viewTitleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+            viewTitleBar.BackgroundColor = Windows.UI.Colors.CornflowerBlue;
+            viewTitleBar.ButtonBackgroundColor = Windows.UI.Colors.CornflowerBlue;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // TODO
+            string myPages = "";
+            foreach (PageStackEntry page in rootFrame.BackStack)
+            {
+                myPages += page.SourcePageType.ToString() + "\n";
+            }
+            if (rootFrame.CanGoBack)
+            {
+                // Show UI in title bar if opted-in and in-app backstack is not empty.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                // Remove the UI from the title bar if in-app back stack is empty.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    AppViewBackButtonVisibility.Collapsed;
+            }
+            var i = new MessageDialog("Welcome!").ShowAsync();
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(title.Text == "" ) {
+                var K = new MessageDialog("Title error!").ShowAsync();
+           }
+            if(details.Text == "")
+            {
+                var K = new MessageDialog("details error!").ShowAsync();
+            }
+            if(date.Date < DateTime.Now.Date){
+                var K = new MessageDialog("dates error!").ShowAsync();
+            }
+            else { }
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            title.Text = "";
+            details.Text = "";
+            date.Date = DateTime.Now.Date;
+        }
+
+        private async void selectButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            BitmapImage new_img = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new System.Uri(file.Path));
+            using (IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+
+                await bitmapImage.SetSourceAsync(fileStream);
+                pic.Source = bitmapImage;
+            }
+
+               
+            
+        }
+        
+    }
+}
